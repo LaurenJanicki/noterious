cc4165dea24c2f266c3c32539fef1dda49571a9f
Lauren Janicki <lauren.a.janicki@gmail.com>
2015-12-16 18:49:01 -0800

    Add controller function and call on click

--- src/app/boards/board-directive.js
+++ src/app/boards/board-directive.js
@@ -1,16 +1,30 @@
 angular.module('noterious')
   .directive('board', function() {
 
+    var link = function(scope, element, attrs) {
+      $(element).hover(function() {
+        $(this).css('color', 'blue');
+      }, function() {
+        $(this).css('color', 'orange');
+      });
+    }
+
+    var controller = function() {
+      var ctrl = this;
+
+      ctrl.count = 0;
+
+      ctrl.incrementCount = function() {
+        ctrl.count ++;
+      }
+    }
+
     return {
       // restrict: 'E',
       scope: true,
       templateUrl: 'app/boards/board-directive.tmpl.html',
-      link: function(scope, element, attrs) {
-        $(element).hover(function() {
-          $(this).css('color', 'blue');
-        }, function() {
-          $(this).css('color', 'orange');
-        });
-      }
+      link: link,
+      controller: controller,
+      controllerAs: 'myCtrl'
     }
   })
